de40632d9209e289d1b7238a27ac8af90ec5de54
Sharol Evanglin <evanglinsharol.27csa@licet.ac.in>
2025-08-08 23:16:39 +0530

    Reapply "Reapply "done""
    This reverts commit 59eabd7f58b4dc9d94ba4501ff4ae0160b97e67e.

--- api-server/config/emailConfig.js
+++ api-server/config/emailConfig.js
@@ -1,8 +1,1 @@
-// Email configuration for nodemailer
-exports.emailConfig = {
-  service: 'gmail',
-  auth: {
-    user: 'evanzzsharol@gmail.com', // replace with your Gmail
-    pass: 'rotw pzlk peva mjxk'      // use App Password, not your real password
-  }
-};
+
--- api-server/utils/otp.js
+++ api-server/utils/otp.js
@@ -1,3 +1,1 @@
-// Utility for OTP generation and storage
-exports.otpStore = {};
-exports.generateOtp = () => Math.floor(100000 + Math.random() * 900000).toString();
+
